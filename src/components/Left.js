5cf89f72ca0ae0d12d8f5bd47ab1fcdac8d489ad
HoYK <ykang.ho@gmail.com>
2023-01-04 15:23:55 +0800

    update 20220104-1

--- src/components/Left.js
+++ src/components/Left.js
@@ -3,33 +3,33 @@ import { Segment, Header, Image, Divider, Container } from "semantic-ui-react";
 import NameItem from "./NameItem";
 
 const Left = () => {
-    return (
-        <div>
-            <Segment>
-                <div style={{ marginTop: "10px" }}>
-                    <Header as="h3">
-                        HO YIKANG
-                        <Header.Subheader>Programmer, Trader</Header.Subheader>
-                    </Header>
-                </div>
-
-                <Image src="images/mypic2.jpg" size="small" circular centered style={{ marginTop: "20px" }} />
-                <Header>
-                    <Header.Subheader>
-                        <Container textAlign="justified">
-                            Passionate about solving problems hidden behind financial market and code.
-                        </Container>
-                    </Header.Subheader>
-                </Header>
-                <Divider />
-
-                <NameItem title="Name" content="Ho Yi Kang" />
-                <NameItem title="Email" content="ykang.ho@gmail.com" />
-                <NameItem title="Cell" content="0926-169-252" />
-                <NameItem title="Skype" content="ykang.ho" />
-            </Segment>
+  return (
+    <div>
+      <Segment>
+        <div style={{ marginTop: "10px" }}>
+          <Header as="h3">
+            HO YI-KANG
+            <Header.Subheader>Programmer, Tech support</Header.Subheader>
+          </Header>
         </div>
-    );
+
+        <Image src="images/mypic2.jpg" size="small" circular centered style={{ marginTop: "20px" }} />
+        <Header>
+          <Header.Subheader>
+            <Container textAlign="justified">
+              {/* Passionate about solving problems hidden behind financial market and code. */}
+            </Container>
+          </Header.Subheader>
+        </Header>
+        <Divider />
+
+        <NameItem title="Name" content="Ho Yi Kang" />
+        <NameItem title="Email" content="ykang.ho@gmail.com" />
+        <NameItem title="Cell" content="0926-169-252" />
+        <NameItem title="Skype" content="ykang.ho" />
+      </Segment>
+    </div>
+  );
 };
 
 export default Left;
